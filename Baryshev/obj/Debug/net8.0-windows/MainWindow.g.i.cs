49ee1e03094158b8f8d37eb97fdb76f86c27312f
lansmaster <stepanbaryhev@gmail.com>
2024-02-08 20:50:29 +0300

    0.2

--- Baryshev/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Baryshev/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace Baryshev {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF_VIS;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Baryshev;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
